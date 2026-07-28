0a29c4420e8c40e9b7fb430f0e78083284a9ce89
mg3994 <manishgautammg7@yahoo.com>
2026-07-28 08:06:28 -0700

    error

--- packages/types-blogger-react/src/index.ts
+++ packages/types-blogger-react/src/index.ts
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as CSS from 'csstype';
+import * as React from "react";
+import * as CSS from "csstype";
 
 // Export CSS namespace for direct utility use in standard projects
 export { CSS };
 
 // Augment React attributes to support Blogger conditional logic, expression binding, and rich CSS inline properties natively!
-declare module 'react' {
+declare module "react" {
   interface HTMLAttributes<T> {
     cond?: string;
     style?: React.CSSProperties;
@@ -19,11 +19,21 @@ declare module 'react' {
 }
 
 // Augmented JSX namespace for Blogger templates that can be used directly or within standard React JSX workflows.
-declare global {
-  namespace JSX {
-    // Custom element interfaces for standard Blogger XML elements
-    interface IntrinsicElements {
-      'b:section': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+export namespace JSX {
+  export type Element = React.JSX.Element;
+  export type ElementClass = React.JSX.ElementClass & IntrinsicElements;
+  export type ElementAttributesProperty = React.JSX.ElementAttributesProperty;
+  export type ElementChildrenAttribute = React.JSX.ElementChildrenAttribute;
+  export type LibraryManagedAttributes<C, P> =
+    React.JSX.LibraryManagedAttributes<C, P>;
+  export type IntrinsicAttributes = React.JSX.IntrinsicAttributes;
+  export type IntrinsicClassAttributes<T> =
+    React.JSX.IntrinsicClassAttributes<T>;
+
+  // Inherit standard React elements and merge Blogger specific tags!
+  export interface IntrinsicElements extends React.JSX.IntrinsicElements {
+    "b:section": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         id: string;
         class?: string;
         maxwidgets?: number | string;
@@ -32,9 +42,12 @@ declare global {
         preferred?: boolean | string;
         cond?: string;
         [exprAttr: `expr:${string}`]: any;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:widget': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:widget": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         id: string;
         type: string;
         title?: string;
@@ -45,74 +58,147 @@ declare global {
         visible?: boolean | string;
         cond?: string;
         [exprAttr: `expr:${string}`]: any;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:widget-settings': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      'b:widget-setting': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { name: string }, HTMLElement>;
+    "b:widget-settings": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+    "b:widget-setting": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { name: string },
+      HTMLElement
+    >;
 
-      'b:if': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { cond: string }, HTMLElement>;
-      'b:elseif': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { cond: string }, HTMLElement>;
-      'b:else': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "b:if": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { cond: string },
+      HTMLElement
+    >;
+    "b:elseif": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { cond: string },
+      HTMLElement
+    >;
+    "b:else": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
 
-      'b:loop': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:loop": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         values: string;
         var: string;
         index?: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:include': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:include": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         name: string;
         data?: string;
         cond?: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:includable': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:includable": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         id: string;
         var?: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:attr': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:attr": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         name: string;
         value: string;
         cond?: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:class': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:class": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         name: string;
         cond: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:tag': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:tag": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         name?: string;
         cond?: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:eval': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { expr: string }, HTMLElement>;
+    "b:eval": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { expr: string },
+      HTMLElement
+    >;
 
-      'b:with': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:with": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         var: string;
         value: string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:switch': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { var: string }, HTMLElement>;
-      'b:case': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { value: string }, HTMLElement>;
-      'b:default': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "b:switch": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { var: string },
+      HTMLElement
+    >;
+    "b:case": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { value: string },
+      HTMLElement
+    >;
+    "b:default": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
 
-      'b:message': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { name: string }, HTMLElement>;
-      'b:comment': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    "b:message": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { name: string },
+      HTMLElement
+    >;
+    "b:comment": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
 
-      'b:template-skin': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-      'b:template-script': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+    "b:template-skin": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+    "b:template-script": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & {
         name: string;
         version: string;
         async?: boolean | string;
-      }, HTMLElement>;
+      },
+      HTMLElement
+    >;
 
-      'b:param': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { value?: string }, HTMLElement>;
+    "b:param": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { value?: string },
+      HTMLElement
+    >;
 
-      'b:defaultmarkup': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & { type: string }, HTMLElement>;
-      'b:defaultmarkups': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-    }
+    "b:defaultmarkup": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement> & { type: string },
+      HTMLElement
+    >;
+    "b:defaultmarkups": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    >;
+
+    // Dynamic tag fallback
+    [elemName: string]: any;
   }
 }
 
---
+++ packages/types-blogger-react/src/jsx-dev-runtime.ts
@@ -0,0 +1,12 @@
+// export { jsx, jsxs, jsxDEV, Fragment } from "./jsx-runtime.js";
+// export * from "./index.js";
+
+// @ts-ignore
+import {
+  jsxDEV as _jsxDEV,
+  Fragment as _Fragment,
+} from "react/jsx-dev-runtime";
+export { JSX } from "./index.js";
+
+export const jsxDEV = _jsxDEV;
+export const Fragment = _Fragment;
